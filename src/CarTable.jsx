516986b4218f1ba9ba8c6d3dd8f49005a387f23d
Matias Nisonen <m.nisonen@gmail.com>
2025-04-03 13:21:24 +0300

    Ag Gridin luonti

--- src/CarTable.jsx
+++ src/CarTable.jsx
@@ -1,6 +1,20 @@
-
+import { AgGridReact } from "ag-grid-react";
+import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 export function CarTable({ cars }) {
+    
+    
+  return (
+    <>
+      <h1>Cars ({cars.length})</h1>
+      <div style={{height: 300, minWidth: 700}}>
+      <AgGridReact 
+            rowData={cars}
+            columnDefs={[{field:"brand"}, {field: "model"}]}
 
-    return <h1>Cars ({cars.length})</h1>
-}
+      />
+      </div>
+    </>
+  );
+}   
